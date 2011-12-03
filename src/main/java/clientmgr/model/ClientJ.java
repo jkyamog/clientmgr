edfd90f241327c716abc57e7500f5b4a349eb28e
Jun Yamog <jkyamog@clear.net.nz>
2011-12-04 12:21:18 +1300

    make jersey and jackson to work

---
+++ src/main/java/clientmgr/model/ClientJ.java
@@ -0,0 +1,20 @@
+package clientmgr.model;
+
+public class ClientJ {
+
+	private String firstName;
+	private String lastName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
--- src/main/scala/clientmgr/rest/ClientResource.scala
+++ src/main/scala/clientmgr/rest/ClientResource.scala
@@ -1,15 +1,21 @@
 package clientmgr.rest
+
 import javax.ws.rs.Path
 import javax.ws.rs.GET
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
+import clientmgr.model._
 
 @Path("/client")
 class ClientResource {
 
 	@GET
+	@Path("/test")
 	@Produces(Array(MediaType.APPLICATION_JSON))
 	def getClients = {
-		"hello"
+		val client = new ClientJ
+		client.setLastName("user")
+		client.setFirstName("test")
+		client
 	}
 }
