45d80695283f0fbebe56f47ac8c94ffc6f9ae7ce
Jun Yamog <jkyamog@clear.net.nz>
2011-12-06 00:25:51 +1300

    get jersey-scala to work, seems to work fine on both scala and java objects

---
+++ src/main/scala/clientmgr/dto/ClientDto.scala
@@ -0,0 +1,3 @@
+package clientmgr.dto
+
+case class ClientDto(firstName: String, lastName: String)
--- src/main/scala/clientmgr/rest/ClientResource.scala
+++ src/main/scala/clientmgr/rest/ClientResource.scala
@@ -5,17 +5,36 @@ import javax.ws.rs.GET
 import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 import clientmgr.model._
+import clientmgr.dto.ClientDto
+import java.util.ArrayList
 
 @Path("/client")
 class ClientResource {
 
 	@GET
-	@Path("/test")
+	@Path("/testscala")
 	@Produces(Array(MediaType.APPLICATION_JSON))
 	def getClients = {
-		val client = new ClientJ
+		Array(ClientDto("user", "scala1"), ClientDto("user", "scala2"))
+	}
+
+	@GET
+	@Path("/testjava")
+	@Produces(Array(MediaType.APPLICATION_JSON))
+	def getClientsJ = {
+		var client = new ClientJ
 		client.setLastName("user")
 		client.setFirstName("test")
-		client
+		
+		val jList = new ArrayList[ClientJ]
+		jList.add(client)
+
+		client = new ClientJ
+		client.setLastName("user2")
+		client.setFirstName("test2")
+		jList.add(client)
+
+		jList
 	}
+
 }
